51d55d4a0cdcff95ed9c4dbba8587b5d334f1049
Jacob Gemma <jacobg391@gmail.com>
2025-03-04 19:19:33 -0500

    Add .gitignore and remove backend files for now (potentially sensitive info)

--- docx_to_pdf.py
+++
@@ -1,7 +0,0 @@
-from docx2pdf import convert
-
-# Batch conversion
-folder_path = r"O:\23013 - Lake Oconee Resort\AQ files & Cutsheets\COVERS\240214\\"
-output_folder = r"O:\23013 - Lake Oconee Resort\AQ files & Cutsheets\COVERS\240214\PDFs"
-
-convert(folder_path, output_folder)
